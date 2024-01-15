62a4fa1a80ebf69aba8ef8c984bb51d971315134
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2024-01-15 19:26:39 -0300

    change: allowed hosts changed

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -155,4 +155,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+]  # os.environ.get("DJANGO_ALLOWED_HOSTS", "").split(" ")
